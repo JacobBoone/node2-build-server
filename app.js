c47f5899db4d70bb5a206eda651b44d7362c92cb
Jacob <jakeboone@aol.com>
2014-07-22 16:00:57 -0600

    Completed Bonus

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 
-var fileContents = fs.readFileSync('data.txt');
-
+// var fileContents = fs.readFileSync('data.txt');
 
 
 var express = require('express');
@@ -10,9 +9,24 @@ var app = express();
 
 
 app.get('/', function(req, res) {
-	res.header('Content-Type', 'text/html');
-	res.send(fileContents);
+	// res.header('Content-Type', 'text/html');
+	// res.send(fileContents);
+	fs.readFile('data.txt', function(err, data){
+  	res.header('Content-Type', 'text/html');
+	res.send(data);
+	})
+
 });
+
+app.get('/:filename', function(req, res) {
+ // access value of :filename with req.params.filename
+ 	var filename = req.params.filename;
+ // read the file with the specified filename
+ 	var filedata = fs.readFileSync('./public/' + filename, 'utf-8');
+	res.send(filedata);
+ // send the contents of the file to the user
+});
+
 
 var server = app.listen(6086, function() {
 	console.log('Express server listening on port ' + server.address().port);
